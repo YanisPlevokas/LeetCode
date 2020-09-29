fd05ec27ac8715597e3a6fbccd05935ce915f537
Yanis Plevokas <58010275+YanisPlevokas@users.noreply.github.com>
2020-09-29 23:39:35 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector <int> num1 = { 1,2 };
+    vector <int> num2 = { 3,4 };
+    vector <int> num3;
+    int i = 0;
+    int j = 0;
+    while (i < num1.size() || j < num2.size())
+    {
+        if (i != num1.size() && j != num2.size())
+        {
+            if (num1[i] < num2[j])
+            {
+                num3.push_back(num1[i++]);
+
+            }
+            else if (num1[i] > num2[j])
+            {
+                num3.push_back(num2[j++]);
+            }
+            else
+            {
+                num3.push_back(num2[j++]);
+                num3.push_back(num1[i++]);
+
+            }
+        }
+        else
+            if (i != num1.size() && j == num2.size())
+            {
+                num3.push_back(num1[i++]);
+            }
+        else
+            if (i == num1.size() && j != num2.size())
+            {
+                num3.push_back(num2[j++]);
+            }
+    }
+    if (num3.size() % 2 != 0)
+        cout << num3[num3.size() / 2];
+    else
+        cout << double((num3[num3.size() / 2 - 1] + num3[num3.size() / 2])) / 2;
+
+
+    return 0;
+    }
